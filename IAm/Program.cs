bf4546fddc66fafc905b05307693f816af3a5388
Noa <nk4110803@gmail.com>
2024-03-10 17:27:50 +0200

    second IAm

--- IAm/Program.cs
+++ IAm/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace IAm
+﻿using System;
+namespace IAm
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("I Am Noa");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("hi "+i);
+            }
         }
     }
 }
